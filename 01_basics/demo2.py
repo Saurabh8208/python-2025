005c21efb0eec4dfaad62f508bef2c5d9b2f0636
Saurabh8208 <saurabhkurwatkar@gmail.com>
2025-09-19 11:51:06 +0530

    variables

---
+++ 01_basics/demo2.py
@@ -0,0 +1,24 @@
+
+
+
+student_name = 'saurabh'
+student_age = 20
+student_gpa = 8.5
+student_passed = 'yes'
+
+print(student_name)
+print(student_age)
+print(student_gpa)
+print(student_passed)
+
+print(id(student_name))
+print(id(student_age))
+print(id(student_gpa))
+print(id(student_passed))
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_gpa))
+print(type(student_passed))
+
+print(student_name+student_passed)
