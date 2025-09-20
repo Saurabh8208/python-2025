0280bc4fe19559857c4d632ddc59ef34ba5be333
Saurabh8208 <saurabhkurwatkar@gmail.com>
2025-09-20 17:06:35 +0530

    variable_types

--- practice_variable/demo3.py
+++ practice_variable/demo3.py
@@ -3,9 +3,9 @@ print(name)
 a = 14
 b = 11
 print(a+b)
-print(id(a))
+print(id(a))#it is a display our variable identity in memory
 print(id(b))
 print(type(a))
-print(type(b))
+print(type(b)) # it is displayy our variable type ex.str,int,float.bool
 print(type(name))
 print(id(name))
